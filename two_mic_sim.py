13762df7cc00db77fe2493e1e9329dcb0e8be539
StefanTesmanovic <tesmanovic166@gmail.com>
2024-07-15 17:29:26 +0200

    ne znam

--- two_mic_sim.py
+++ two_mic_sim.py
@@ -1,21 +1,23 @@
 import pygame
 import sys
+import math
 
 # Initialize Pygame
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 1201, 801
+WIDTH, HEIGHT = 1200, 600
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+a = 1
 
 # Set up the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simulation")
 
 # Initial position of the point
-x, y = WIDTH // 2, HEIGHT // 2
-
+x, y = 0, 0
+center_x, center_y = WIDTH // 2, HEIGHT // 2
 # Game loop
 while True:
     for event in pygame.event.get():
@@ -30,9 +32,9 @@ while True:
     if keys[pygame.K_RIGHT]:
         x += 1
     if keys[pygame.K_UP]:
-        y -= 1
-    if keys[pygame.K_DOWN]:
         y += 1
+    if keys[pygame.K_DOWN]:
+        y -= 1
 
     # Draw the coordinate system
     screen.fill(WHITE)
@@ -40,10 +42,12 @@ while True:
     pygame.draw.line(screen, RED, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), 1)
 
     # Draw the point
-    pygame.draw.circle(screen, RED, (x, y), 3)
+    pygame.draw.circle(screen, RED, (center_x + x, center_y - y), 3)
 
     # Update the display
     pygame.display.flip()
 
     # Set the frame rate
     pygame.time.Clock().tick(30)
+
+    print(x, " ", y, ", r = ", math.sqrt(x*x + y*y), "th = ", math.atan2(x,y)*180/math.pi)        
