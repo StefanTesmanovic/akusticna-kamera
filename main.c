38d575c593f53c6c739a46fb241f90bfdb0f69bc
StefanTesmanovic <tesmanovic166@gmail.com>
2024-07-20 10:07:48 +0200

    <poruka>

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdint.h>
 
 //global variables
-long double timeInc = 0.00001;
+float timeInc = 0.0001;float t = 0;// sample rate 10KHz
 float a = 0.5; // [cm]
 float A = 1; // amplituda
 int c = 33000; // [cm/s]
@@ -12,13 +13,16 @@ long double r = 300; //[cm]
 long double th = 45; // stepeni
 long double d1, d2; // razdaljine mikrofona od izvora
 
-int mic1[90000], mic2[90000];
+uint16_t mic1[4096], mic2[4096]; // izabrana duzina koja je stepen dvojike zbog fft
 
 int main(){
     long double thRad = (45/180)*M_PI;
-
-    for(int i = 0; i < 90000; i++){
-
+    d1 = sqrt(r*r - r*cos(thRad) + 0.25);
+    d2 = sqrt(r*r + r*cos(thRad) + 0.25);
+    for(int i = 0; i < 4096; i++){
+        mic1[i] = ((A/d1)*sin(2*M_PI*freq*(t-d1/c)) + 1)*2048;
+        mic2[i] = ((A/d2)*sin(2*M_PI*freq*(t-d2/c)) + 1)*2048;
+        t += timeInc;
     }
     return 0;
 }
